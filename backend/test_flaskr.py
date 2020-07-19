b33827bc264273662805876014517ca8d4b7e74d
TheTrueSoul <albgumifahd@gmail.com>
2020-07-19 03:49:32 +0300

    added more tests and fixed earlier tests

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -15,8 +15,17 @@ class TriviaTestCase(unittest.TestCase):
         self.app = create_app()
         self.client = self.app.test_client
         self.database_name = "trivia_test"
-        self.database_path = "postgres://{}/{}".format('localhost:5432', self.database_name)
+        self.database_path = "postgres://{}/{}".format(
+            'localhost:5432', self.database_name)
         setup_db(self.app, self.database_path)
+
+        # sample question for use in tests
+        self.new_question = {
+            'question': 'Which four states make up the 4 Corners region of the US?',
+            'answer': 'Colorado, New Mexico, Arizona, Utah',
+            'difficulty': 3,
+            'category': '3'
+        }
 
         # binds the app to the current context
         with self.app.app_context():
@@ -24,15 +33,11 @@ class TriviaTestCase(unittest.TestCase):
             self.db.init_app(self.app)
             # create all tables
             self.db.create_all()
-    
+
     def tearDown(self):
         """Executed after reach test"""
         pass
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
     def test_get_paginated_questions(self):
         """Tests question pagination success"""
 
@@ -47,6 +52,18 @@ class TriviaTestCase(unittest.TestCase):
         # check that total_questions and questions return data
         self.assertTrue(data['total_questions'])
         self.assertTrue(len(data['questions']))
+
+    def test_404_request_beyond_valid_page(self):
+        """Tests question pagination failure 404"""
+
+        # send request with bad page data, load response
+        response = self.client().get('/questions?page=100')
+        data = json.loads(response.data)
+
+        # check status code and message
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
 
     def test_delete_question(self):
         """Tests question deletion success"""
@@ -111,6 +128,26 @@ class TriviaTestCase(unittest.TestCase):
         # check that question is not None
         self.assertIsNotNone(question)
 
+    def test_422_if_question_creation_fails(self):
+        """Tests question creation failure 422"""
+
+        # get number of questions before post
+        questions_before = Question.query.all()
+
+        # create new question without json data, then load response data
+        response = self.client().post('/questions', json={})
+        data = json.loads(response.data)
+
+        # get number of questions after post
+        questions_after = Question.query.all()
+
+        # check status code and success message
+        self.assertEqual(response.status_code, 422)
+        self.assertEqual(data['success'], False)
+
+        # check if questions_after and questions_before are equal
+        self.assertTrue(len(questions_after) == len(questions_before))
+
     def test_search_questions(self):
         """Tests search questions success"""
 
@@ -131,6 +168,21 @@ class TriviaTestCase(unittest.TestCase):
         # check that id of question in response is correct
         self.assertEqual(data['questions'][0]['id'], 23)
 
+    def test_404_if_search_questions_fails(self):
+        """Tests search questions failure 404"""
+
+        # send post request with search term that should fail
+        response = self.client().post('/questions',
+                                      json={'searchTerm': 'abcdefghijk'})
+
+        # load response data
+        data = json.loads(response.data)
+
+        # check response status code and message
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'resource not found')
+
     def test_get_questions_by_category(self):
         """Tests getting questions by category success"""
 
@@ -149,6 +201,21 @@ class TriviaTestCase(unittest.TestCase):
 
         # check that current category returned is science
         self.assertEqual(data['current_category'], 'Science')
+
+    def test_400_if_questions_by_category_fails(self):
+        """Tests getting questions by category failure 400"""
+
+        # send request with category id 100
+        response = self.client().get('/categories/100/questions')
+
+        # load response data
+        data = json.loads(response.data)
+
+        # check response status code and message
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(data['success'], False)
+        self.assertEqual(data['message'], 'bad request')
+
     def test_play_quiz_game(self):
         """Tests playing quiz game success"""
 
